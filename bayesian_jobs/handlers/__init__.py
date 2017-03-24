62935afe206ec8dff3436471c5c0527d811fd72e
Jiri Popelka <jpopelka@redhat.com>
2017-03-24 15:39:26 +0100

    POST /jobs/popular-analyses: analyse top maven/npm packages

--- bayesian_jobs/handlers/__init__.py
+++ bayesian_jobs/handlers/__init__.py
@@ -2,4 +2,6 @@
 
 from .error import ErrorHandler
 from .flow import FlowScheduling
+from .maven_popular_analyses import MavenPopularAnalyses
+from .npm_popular_analyses import NpmPopularAnalyses
 from .selective_flow import SelectiveFlowScheduling
