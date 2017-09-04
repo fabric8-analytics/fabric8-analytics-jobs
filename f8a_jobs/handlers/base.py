480d09fbd72937a83d81c52c62152175c29aba38
Fridolin Pokorny <fridolin@redhat.com>
2017-09-04 11:46:45 +0200

    Perform copy when expanding filter queries

--- f8a_jobs/handlers/base.py
+++ f8a_jobs/handlers/base.py
@@ -2,6 +2,7 @@
 
 import re
 import logging
+import copy
 from collections import namedtuple
 from selinon import run_flow
 from selinon import run_flow_selective
@@ -138,6 +139,8 @@ class BaseHandler(object):
         :param filter_definition:
         :return: expanded filter arguments
         """
+        # As filters of periodic jobs are stored in memory, copy filter definition so the original is not overwritten
+        filter_definition = copy.deepcopy(filter_definition)
         select_statement = self.construct_select_query(filter_definition.pop(self.DEFAULT_FILTER_KEY))
         query_result = self.postgres.session.execute(select_statement).fetchall()
 
