8e621651136d2ca1476e435f666affbc612de79c
Fridolin Pokorny <fridolin@redhat.com>
2017-11-02 18:14:34 +0100

    Respect review comments

--- f8a_jobs/handlers/clean_postgres.py
+++ f8a_jobs/handlers/clean_postgres.py
@@ -1,12 +1,25 @@
 from selinon import StoragePool
 from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy import or_
 from f8a_worker.models import WorkerResult, Analysis, PackageAnalysis, PackageWorkerResult
 
 from .base import BaseHandler
 
 
 class CleanPostgres(BaseHandler):
-    """Clean JSONB columns in Postgres."""
+    """Clean JSONB columns in Postgres.
+
+    As we store results in Postgres/RDS temporary, they live there only till
+    ResultCollector (or PackageResultCollector) is executed. After that all JSONB columns
+    that store task results are replaced with a JSON containing a single key version_id that
+    corresponds to version of JSON file stored on S3 (task result).
+
+    If for some reason the flow is interrupted, these data can be still present in the RDS. This
+    class is supposed to remove such entries and replace them with the latest version id (we don't
+    know exact version).
+    """
+
+    _SLICE_SIZE = 10
 
     def _clean_package_version_data(self, from_date, to_date, clean_unfinished):
         s3 = StoragePool.get_connected_storage('S3Data')
@@ -16,19 +29,22 @@ class CleanPostgres(BaseHandler):
             filter(WorkerResult.external_request_id.is_(None)).\
             order_by(WorkerResult.id)
 
-        if not clean_unfinished:
-            query = query.filter(Analysis.finished_at.isnot(None))
-
         if from_date:
             query = query.filter(Analysis.started_at >= from_date)
 
         if to_date:
-            query = query.filter(Analysis.finished_at <= to_date)
+            if not clean_unfinished:
+                query = query.filter(Analysis.finished_at <= to_date)
+            else:
+                query = query.filter(or_(Analysis.finished_at.is_(None),
+                                         Analysis.finished_at <= to_date))
+        elif not clean_unfinished:
+            query = query.filter(Analysis.finished_at.isnot(None))
 
         start = 0
         while True:
             try:
-                results = query.slice(start, start + 10).all()
+                results = query.slice(start, start + self._SLICE_SIZE).all()
             except SQLAlchemyError:
                 self.postgres.session.rollback()
                 raise
@@ -38,7 +54,7 @@ class CleanPostgres(BaseHandler):
                 break
 
             self.log.info("Updating results, slice offset is %s", start)
-            start += 10
+            start += self._SLICE_SIZE
 
             for entry in results:
                 if entry.worker[0].isupper() or \
@@ -76,19 +92,22 @@ class CleanPostgres(BaseHandler):
             filter(PackageWorkerResult.external_request_id.is_(None)).\
             order_by(PackageWorkerResult.id)
 
-        if not clean_unfinished:
-            query = query.filter(PackageAnalysis.finished_at.isnot(None))
-
         if from_date:
             query = query.filter(PackageAnalysis.started_at >= from_date)
 
         if to_date:
-            query = query.filter(PackageAnalysis.finished_at <= to_date)
+            if not clean_unfinished:
+                query = query.filter(PackageAnalysis.finished_at <= to_date)
+            else:
+                query = query.filter(or_(PackageAnalysis.finished_at.is_(None),
+                                         PackageAnalysis.finished_at <= to_date))
+        elif not clean_unfinished:
+            query = query.filter(PackageAnalysis.finished_at.isnot(None))
 
         start = 0
         while True:
             try:
-                results = query.slice(start, start + 10).all()
+                results = query.slice(start, start + self._SLICE_SIZE).all()
             except SQLAlchemyError:
                 self.postgres.session.rollback()
                 raise
@@ -98,7 +117,7 @@ class CleanPostgres(BaseHandler):
                 break
 
             self.log.info("Updating results, slice offset is %s", start)
-            start += 10
+            start += self._SLICE_SIZE
 
             for entry in results:
                 if entry.worker[0].isupper() or \
@@ -134,6 +153,6 @@ class CleanPostgres(BaseHandler):
 
         self.log.info("Cleaning computed data in package level flow")
         self._clean_package_data(from_date, to_date, clean_unfinished)
-        self.log.info("Cleaning computed data in package level flows")
+        self.log.info("Cleaning computed data in package-version level flows")
         self._clean_package_version_data(from_date, to_date, clean_unfinished)
         self.log.info("Cleaning has successfully finished")
