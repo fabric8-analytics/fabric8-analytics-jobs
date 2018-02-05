6e45b14816f632e63315c8902ea534ed42a399ff
Jiri Popelka <jpopelka@redhat.com>
2018-02-05 13:06:06 +0100

    Extract repeating code into a decorator function

--- f8a_jobs/handlers/book_keeping.py
+++ f8a_jobs/handlers/book_keeping.py
@@ -1,11 +1,30 @@
 """Class to retrieve BookKeeping data."""
 
+from functools import wraps
 from selinon import StoragePool
 from f8a_worker.models import (Analysis, Ecosystem, Package, Version,
                                WorkerResult, PackageWorkerResult, PackageAnalysis,
                                Upstream)
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.exc import SQLAlchemyError
+
+
+def handle_sqlalchemy(func):
+    """Decorate repeating code."""
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        """Wrap sqlalchemy code into try-except."""
+        try:
+            data = func(*args, **kwargs)
+            result = {"summary": data}
+        except NoResultFound:
+            result = {"error": "No result found."}
+        except SQLAlchemyError:
+            StoragePool.get_connected_storage('BayesianPostgres').session.rollback()
+            result = {"error": "SQLAlchemyError encountered while fetching data. "
+                               "Roll-backing. Try again."}
+        return result
+    return wrapper
 
 
 class BookKeeping(object):
@@ -16,96 +35,70 @@ class BookKeeping(object):
         rdb = StoragePool.get_connected_storage('BayesianPostgres')
         self.db = rdb.session
 
+    @handle_sqlalchemy
     def retrieve_bookkeeping_all(self):
         """Retrieve BookKeeping data for all Ecosystems."""
-        try:
-            data = []
-            for e in self.db.query(Ecosystem).all():
-                package_count = self.db.query(Package).filter(Package.ecosystem == e).count()
-                ecosystem_name = self.db.query(Ecosystem).get(e.id).name
-                pv_count = self.db.query(Version).join(Package).\
-                    filter(Package.ecosystem == e).count()
-                entry = {
-                    "name": ecosystem_name,
-                    "package_count": package_count,
-                    "package_version_count": pv_count
-                }
-                data.append(entry)
-            result = {"summary": data}
-        except SQLAlchemyError:
-            self.db.session.rollback()
-            result = {"error": "Error encountered while fetching data. Please check logs."}
+        data = []
+        for e in self.db.query(Ecosystem).all():
+            package_count = self.db.query(Package).filter(Package.ecosystem == e).count()
+            ecosystem_name = self.db.query(Ecosystem).get(e.id).name
+            pv_count = self.db.query(Version).join(Package).\
+                filter(Package.ecosystem == e).count()
+            entry = {
+                "name": ecosystem_name,
+                "package_count": package_count,
+                "package_version_count": pv_count
+            }
+            data.append(entry)
+        return data
 
-        return result
-
+    @handle_sqlalchemy
     def retrieve_bookkeeping_for_ecosystem(self, ecosystem):
         """Retrieve BookKeeping data for given Ecosystem.
 
         :param ecosystem: ecosystem for which the data should be retrieved
         """
-        try:
-            e = Ecosystem.by_name(self.db, ecosystem)
-            package_count = self.db.query(Package).filter(Package.ecosystem == e).count()
-            pv_count = self.db.query(Version).join(Package).filter(Package.ecosystem == e).count()
-            result = {
-                "summary": {
-                    "ecosystem": e.name,
-                    "package_count": package_count,
-                    "package_version_count": pv_count
-                }
-            }
-        except NoResultFound:
-            result = {"error": "No such ecosystem: %s" % ecosystem}
-        except SQLAlchemyError:
-            self.db.session.rollback()
-            result = {"error": "Error encountered while fetching data. Please check logs."}
+        e = Ecosystem.by_name(self.db, ecosystem)
+        package_count = self.db.query(Package).filter(Package.ecosystem == e).count()
+        pv_count = self.db.query(Version).join(Package).filter(Package.ecosystem == e).count()
+        return {"ecosystem": e.name,
+                "package_count": package_count,
+                "package_version_count": pv_count}
 
-        return result
-
+    @handle_sqlalchemy
     def retrieve_bookkeeping_for_ecosystem_package(self, ecosystem, package):
         """Retrieve BookKeeping data for given Package and Ecosystem.
 
         :param ecosystem: ecosystem for which the data should be retrieved
         :param package: package for which the data should be retrieved
         """
-        try:
-            e = Ecosystem.by_name(self.db, ecosystem)
-            p = Package.by_name(self.db, package)
-            version_count = self.db.query(Version).join(Package).\
-                filter(Package.ecosystem == e).\
-                filter(Version.package == p).count()
-            stat = self.db.query(PackageWorkerResult.worker, PackageWorkerResult.error,
-                                 PackageWorkerResult.task_result).join(PackageAnalysis). \
-                filter(PackageAnalysis.package == p).all()
+        e = Ecosystem.by_name(self.db, ecosystem)
+        p = Package.by_name(self.db, package)
+        version_count = self.db.query(Version).join(Package).\
+            filter(Package.ecosystem == e).\
+            filter(Version.package == p).count()
+        stat = self.db.query(PackageWorkerResult.worker, PackageWorkerResult.error,
+                             PackageWorkerResult.task_result).join(PackageAnalysis). \
+            filter(PackageAnalysis.package == p).all()
 
-            worker_stats = []
-            for worker_name, has_error, task_result in stat:
-                entry = {"worker_name": worker_name,
-                         "has_error": has_error,
-                         "task_result": task_result}
-                worker_stats.append(entry)
+        worker_stats = []
+        for worker_name, has_error, task_result in stat:
+            entry = {"worker_name": worker_name,
+                     "has_error": has_error,
+                     "task_result": task_result}
+            worker_stats.append(entry)
 
-            p_versions = self.db.query(Version).join(Package).join(Ecosystem).\
-                filter(Package.ecosystem == e).\
-                filter(Version.package == p)
+        p_versions = self.db.query(Version).join(Package).join(Ecosystem).\
+            filter(Package.ecosystem == e).\
+            filter(Version.package == p)
 
-            result = {
-                "summary": {
-                    "ecosystem": e.name,
-                    "package": p.name,
-                    "package_version_count": version_count,
-                    "package_level_workers": worker_stats,
-                    "analysed_versions": [v.identifier for v in p_versions]
-                }
-            }
-        except NoResultFound:
-            result = {"error": "No such package: %s/%s" % (ecosystem, package)}
-        except SQLAlchemyError:
-            self.db.session.rollback()
-            result = {"error": "Error encountered while fetching data. Please check logs."}
+        return {"ecosystem": e.name,
+                "package": p.name,
+                "package_version_count": version_count,
+                "package_level_workers": worker_stats,
+                "analysed_versions": [v.identifier for v in p_versions]}
 
-        return result
-
+    @handle_sqlalchemy
     def retrieve_bookkeeping_for_epv(self, ecosystem, package, version):
         """Retrieve BookKeeping data for the given ecosystem, package, and version.
 
@@ -113,61 +106,44 @@ class BookKeeping(object):
         :param package: package for which the data should be retrieved
         :param version: package version for which the data should be retrieved
         """
-        try:
-            e = Ecosystem.by_name(self.db, ecosystem)
-            p = Package.by_name(self.db, package)
-            v = self.db.query(Version).join(Package).join(Ecosystem). \
-                filter(Package.ecosystem == e). \
-                filter(Version.package == p). \
-                filter(Version.identifier == version).one()
-            stat = self.db.query(WorkerResult.worker,
-                                 WorkerResult.error,
-                                 WorkerResult.task_result).join(Analysis).join(Version).\
-                filter(Analysis.version == v).all()
+        e = Ecosystem.by_name(self.db, ecosystem)
+        p = Package.by_name(self.db, package)
+        v = self.db.query(Version).join(Package).join(Ecosystem). \
+            filter(Package.ecosystem == e). \
+            filter(Version.package == p). \
+            filter(Version.identifier == version).one()
+        stat = self.db.query(WorkerResult.worker,
+                             WorkerResult.error,
+                             WorkerResult.task_result).join(Analysis).join(Version).\
+            filter(Analysis.version == v).all()
 
-            worker_stats = []
-            for worker_name, has_error, task_result in stat:
-                entry = {"worker_name": worker_name,
-                         "has_error": has_error,
-                         "task_result": task_result}
-                worker_stats.append(entry)
+        worker_stats = []
+        for worker_name, has_error, task_result in stat:
+            entry = {"worker_name": worker_name,
+                     "has_error": has_error,
+                     "task_result": task_result}
+            worker_stats.append(entry)
 
-            result = {
-                "summary": {
-                    "ecosystem": e.name,
-                    "package": p.name,
-                    "version": v.identifier,
-                    "workers": worker_stats
-                }
-            }
-        except NoResultFound:
-            return {"error": "No such version: %s/%s/%s" % (ecosystem, package, version)}
-        except SQLAlchemyError:
-            self.db.session.rollback()
-            result = {"error": "Error encountered while fetching data. Please check logs."}
+        return {"ecosystem": e.name,
+                "package": p.name,
+                "version": v.identifier,
+                "workers": worker_stats}
 
-        return result
-
+    @handle_sqlalchemy
     def retrieve_bookkeeping_upstreams(self):
         """Retrieve BookKeeping data for monitored upstreams."""
-        try:
-            data = []
-            query = self.db.query(Upstream, Package.name, Ecosystem.name).\
-                filter(Package.id == Upstream.package_id).\
-                filter(Ecosystem.id == Package.ecosystem_id)
-            for upstream, package, ecosystem in query.all():
-                entry = {
+        data = []
+        query = self.db.query(Upstream, Package.name, Ecosystem.name).\
+            filter(Package.id == Upstream.package_id).\
+            filter(Ecosystem.id == Package.ecosystem_id)
+        for upstream, package, ecosystem in query.all():
+            entry = {
                     "ecosystem_package": "{e}/{p}".format(e=ecosystem, p=package),
                     "url": upstream.url,
                     "updated_at": upstream.updated_at,
                     "added_at": upstream.added_at,
                     "deactivated_at": upstream.deactivated_at,
                     "active": upstream.active
-                }
-                data.append(entry)
-            result = {"summary": data}
-        except SQLAlchemyError:
-            self.db.session.rollback()
-            result = {"error": "Error encountered while fetching data. Please check logs."}
-
-        return result
+            }
+            data.append(entry)
+        return data
