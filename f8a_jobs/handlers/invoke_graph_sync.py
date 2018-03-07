e240d5323086a5e8e4df407d68e50abdf6221598
Saleem Ansari <tuxdna@gmail.com>
2018-03-07 14:27:44 +0530

    Use concurrent.futures to make async requests

--- f8a_jobs/handlers/invoke_graph_sync.py
+++ f8a_jobs/handlers/invoke_graph_sync.py
@@ -1,13 +1,9 @@
 """Sync all pending packages to Graph DB."""
 
-from sqlalchemy.exc import SQLAlchemyError
-from f8a_worker.models import Analysis, Package, Version, Ecosystem
-from f8a_worker.workers import GraphImporterTask
-from f8a_worker.base import BaseTask
-import requests
 from os import environ
 import urllib
-
+import concurrent.futures
+import requests
 
 from .base import BaseHandler
 
@@ -30,20 +26,38 @@ class InvokeGraphSync(BaseHandler):
     _INGEST_API_URL = "http://{host}:{port}/{endpoint}".format(
         host=_SERVICE_HOST, port=_SERVICE_PORT, endpoint=_INGEST_SERVICE_ENDPOINT)
 
-    query_slice = 10
+    BATCH_SIZE = 10
 
     def _fetch_all_counts(self, params={}):
-        url = "%s/?%s" % (self._PENDING_API_URL, urllib.parse.urlencode(params))
+        url = "%s?%s" % (self._PENDING_API_URL, urllib.parse.urlencode(params))
+        self.log.info(url)
         response = requests.get(url)
+        self.log.info(response)
         data = response.json()
         all_counts = data["all_counts"]
+        self.log.info(data)
         return all_counts
 
     def _fetch_package_versions(self, params={}, offset=None, limit=None):
-        url = "%s/?%s" % (self._PENDING_API_URL, urllib.parse.urlencode(params))
+        request_params = params.copy()
+        request_params.update({"offset": offset, "limit": limit})
+        url = "%s?%s" % (self._PENDING_API_URL, urllib.parse.urlencode(request_params))
+        self.log.info(url)
         response = requests.get(url)
+        self.log.info(response)
         data = response.json()
         return data["pending_list"]
+
+    def _perform_sync(self, packages_list):
+        self.log.info("Invoke graph importer at url: '%s' for %s", self._INGEST_API_URL,
+                      packages_list)
+        response = requests.post(self._INGEST_API_URL, json=packages_list)
+
+        if response.status_code != 200:
+            raise RuntimeError("Failed to invoke graph import at '%s' for %s" % (
+                self._INGEST_API_URL, packages_list))
+
+        self.log.info("Graph import succeeded with response: %s", response.text)
 
     def execute(self, **kwargs):
         """Start the synchronization of all finished analyses to Graph database."""
@@ -51,33 +65,37 @@ class InvokeGraphSync(BaseHandler):
         # fetch count of pending list
         #
         # fetch windowed pending list from backend
-        # start with offset at 0, batch_size = query_slice
+        # start with offset at 0, batch_size = BATCH_SIZE
         # while there are more records
         # batch = fetch current batch
         # schedule graph sync for current batch
         # increase offset by query_slice
         # end
+        self.log.info(kwargs)
 
-        all_counts = self._fetch_all_counts(params)
-        offset = 0
-        while all_counts > 0:
-            # for each batch of packages, send request to backend for ingesting this batch
+        future_to_params_map = {}
 
-            # package_list = [
-            #     {'ecosystem': 'npm', 'name': 'serve-static', 'version': '1.1.7'},...
-            # ]
-            packages_list = self._fetch_package_versions(params=kwargs, offset=offset,
-                                         limit=self.query_slice)
+        with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
+            all_counts = self._fetch_all_counts(params=kwargs)
+            self.log.info(all_counts)
+            offset = 0
+            while all_counts > offset:
+                # for each batch of packages, send request to backend for ingesting this batch
 
-            self.log.info("Invoke graph importer at url: '%s' for %s", self._INGEST_API_URL,
-                          packages_list)
-            # response = requests.post(self._INGEST_API_URL, json=packages_list)
-            #
-            # if response.status_code != 200:
-            #     raise RuntimeError("Failed to invoke graph import at '%s' for %s" % (
-            #         self._INGEST_API_URL, packages_list))
+                # package_list = [
+                #     {'ecosystem': 'npm', 'name': 'serve-static', 'version': '1.1.7'},...
+                # ]
+                packages_list = self._fetch_package_versions(params=kwargs, offset=offset,
+                                                             limit=self.BATCH_SIZE)
+                executor.submit(self._perform_sync, packages_list)
+                offset += self.BATCH_SIZE
 
-            # self.log.info("Graph import succeeded with response: %s", response.text)
-            offset += self.query_slice
-
-
+            for future in concurrent.futures.as_completed(future_to_params_map):
+                request_data = future_to_params_map[future]
+                try:
+                    response_data = future.result()
+                except Exception as exc:
+                    print("FAILURE: %s" % response_data)
+                    print('%r generated an exception: %s' % (response_data, exc))
+                else:
+                    print("SUCCESS: %s" % request_data)
