3e8a061b1f6611058675e2c8cd2a48880d4109c7
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-04-09 09:21:34 +0200

    Stub for the API v1 tests

---
+++ tests/test_api_v1.py
@@ -0,0 +1,17 @@
+"""Tests for the module 'api_v1'."""
+
+import pytest
+
+from f8a_jobs.api_v1 import *
+
+
+class TestApiV1Functions(object):
+    """Tests for the module 'api_v1'."""
+
+    def setup_method(self, method):
+        """Set up any state tied to the execution of the given method in a class."""
+        pass
+
+    def teardown_method(self, method):
+        """Teardown any state that was previously setup with a setup_method call."""
+        pass
