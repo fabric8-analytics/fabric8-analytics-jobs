f81d54c92e21189a305a546934ea2f31ecefeee7
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-09-06 11:45:06 +0200

    Make code checkers happy

--- f8a_jobs/handlers/__init__.py
+++ f8a_jobs/handlers/__init__.py
@@ -19,3 +19,24 @@ from .python_popular_analyses import PythonPopularAnalyses
 from .selective_flow import SelectiveFlowScheduling
 from .sync_to_graph import SyncToGraph
 from .invoke_graph_sync import InvokeGraphSync
+
+# make code checkers happy
+assert AggregateCrowdSourceTags is not None
+assert AggregateGitHubManifestPackages is not None
+assert AggregateTopics is not None
+assert BookKeeping is not None
+assert CleanPostgres is not None
+assert ErrorHandler is not None
+assert FlowScheduling is not None
+assert GitHubMostStarred is not None
+assert GitHubManifests is not None
+assert GolangPopularAnalyses is not None
+assert KronosDataUpdater is not None
+assert MavenPopularAnalyses is not None
+assert MavenReleasesAnalyses is not None
+assert NpmPopularAnalyses is not None
+assert NugetPopularAnalyses is not None
+assert PythonPopularAnalyses is not None
+assert SelectiveFlowScheduling is not None
+assert SyncToGraph is not None
+assert InvokeGraphSync is not None
