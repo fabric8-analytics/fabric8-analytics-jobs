93d617475bdb50618d29ee2b76af4ebaca69abf2
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-03-26 14:12:06 +0100

    Stub for tests for aggregate_topic

---
+++ tests/handlers/test_aggregate_topics.py
@@ -0,0 +1,18 @@
+"""Tests for aggregate_topics.py."""
+
+import pytest
+
+# TODO enable when new test(s) will be added
+# from f8a_jobs.handlers.aggregate_topics import *
+
+
+class TestAggreateTopics(object):
+    """Tests for aggregate_topics.py."""
+
+    def setup_method(self, method):
+        """Set up any state tied to the execution of the given method in a class."""
+        assert method
+
+    def teardown_method(self, method):
+        """Teardown any state that was previously setup with a setup_method call."""
+        assert method
