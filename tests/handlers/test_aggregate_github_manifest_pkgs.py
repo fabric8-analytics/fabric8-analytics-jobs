b26ad0ad35056544a353645aab92c4b1ac44ccb5
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-03-26 14:11:53 +0100

    Stub for tests for aggregate_github_manifest_pkgs.py

---
+++ tests/handlers/test_aggregate_github_manifest_pkgs.py
@@ -0,0 +1,18 @@
+"""Tests for aggregate_github_manifest_pkgs.py."""
+
+import pytest
+
+# TODO enable when new test(s) will be added
+# from f8a_jobs.handlers.aggregate_github_manifest_pkgs import *
+
+
+class TestAggreateGithubManifestPkgs(object):
+    """Tests for aggregate_github_manifest_pkgs.py."""
+
+    def setup_method(self, method):
+        """Set up any state tied to the execution of the given method in a class."""
+        assert method
+
+    def teardown_method(self, method):
+        """Teardown any state that was previously setup with a setup_method call."""
+        assert method
