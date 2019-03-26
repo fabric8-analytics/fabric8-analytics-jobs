66981598f7b4f14a86d153c8db493e5991fb73e7
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-03-26 14:26:37 +0100

    Stub for tests for maven_popular_analyses.py

---
+++ tests/handlers/test_maven_popular_analyses.py
@@ -0,0 +1,11 @@
+"""Tests for MavenPopularAnalyses class."""
+
+from f8a_jobs.handlers.maven_popular_analyses import MavenPopularAnalyses
+
+
+class TestMavenPopularAnalyses(object):
+    """Tests for MavenPopularAnalyses class."""
+
+    def test_constructor(self):
+        """Test the MavenPopularAnalyses() constructor."""
+        assert MavenPopularAnalyses() is not None
