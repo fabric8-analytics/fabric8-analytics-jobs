9986fcfb84e8ccd43e58c8b1724fd53fee108d70
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-03-26 14:27:23 +0100

    Stub for tests for python_popular_analyses.py

---
+++ tests/handlers/test_python_popular_analyses.py
@@ -0,0 +1,11 @@
+"""Tests for PythonPopularAnalyses class."""
+
+from f8a_jobs.handlers.python_popular_analyses import PythonPopularAnalyses
+
+
+class TestPythonPopularAnalyses(object):
+    """Tests for PythonPopularAnalyses class."""
+
+    def test_constructor(self):
+        """Test the PythonPopularAnalyses() constructor."""
+        assert PythonPopularAnalyses() is not None
