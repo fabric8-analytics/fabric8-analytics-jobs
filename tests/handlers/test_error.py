474d9f4c7339c0cc1d6fb09c9595caddafea1597
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-03-26 14:12:46 +0100

    Stub for tests for error.py

---
+++ tests/handlers/test_error.py
@@ -0,0 +1,18 @@
+"""Tests for error.py."""
+
+import pytest
+
+# TODO enable when new test(s) will be added
+# from f8a_jobs.handlers.error import ErrorHandler
+
+
+class TestErrorHandler(object):
+    """Tests for ErrorHandler class."""
+
+    def setup_method(self, method):
+        """Set up any state tied to the execution of the given method in a class."""
+        assert method
+
+    def teardown_method(self, method):
+        """Teardown any state that was previously setup with a setup_method call."""
+        assert method
