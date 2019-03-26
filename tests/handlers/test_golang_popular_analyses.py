dfff492fe59bf2dad3c209dc5a26a91160ce9508
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-03-26 14:26:24 +0100

    Stub for tests for golang_popular_analyses.py

---
+++ tests/handlers/test_golang_popular_analyses.py
@@ -0,0 +1,11 @@
+"""Tests for GolangPopularAnalyses class."""
+
+from f8a_jobs.handlers.golang_popular_analyses import GolangPopularAnalyses
+
+
+class TestGolangPopularAnalyses(object):
+    """Tests for GolangPopularAnalyses class."""
+
+    def test_constructor(self):
+        """Test the GolangPopularAnalyses() constructor."""
+        assert GolangPopularAnalyses() is not None
