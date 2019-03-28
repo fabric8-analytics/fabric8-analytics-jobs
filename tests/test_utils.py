aedb48ad2b80987f5d8db7103b0f4c8621b05670
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-03-28 13:41:47 +0100

    Additional unit test for the parse_date function

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -44,3 +44,21 @@ class TestUtilFunctions(object):
                       'to_date': to_date}
         with pytest.raises(ValueError):
             parse_dates(job_kwargs)
+
+    def test_parse_dates_return_value(self):
+        """Test for the function parse_dates."""
+        job_kwargs = {}
+        parse_dates(job_kwargs)
+        assert job_kwargs == {}
+
+        from_date = '2017-01-01'
+        to_date = '2018-01-01'
+        job_kwargs = {'from_date': from_date,
+                      'to_date': to_date}
+        parse_dates(job_kwargs)
+
+        assert 'from_date' in job_kwargs
+        assert 'to_date' in job_kwargs
+
+        assert job_kwargs['from_date'] == '2017-01-01'
+        assert job_kwargs['to_date'] == '2018-01-01'
