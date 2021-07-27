b627d6371c9c310b54c429210c6cffc6c15b7add
jparsai <jparsai@redhat.com>
2021-07-27 14:51:28 +0530

    chore: New API to listing PVC files (#424)

--- f8a_jobs/defaults.py
+++ f8a_jobs/defaults.py
@@ -24,6 +24,8 @@ DEPLOYMENT_PREFIX = os.getenv('DEPLOYMENT_PREFIX', os.getenv('USER'))
 
 # keep disabled authentication by default
 DISABLE_AUTHENTICATION = os.getenv('DISABLE_AUTHENTICATION', '1') in ('1', 'True', 'true')
+TOKEN_AUTHENTICATION = os.getenv('TOKEN_AUTHENTICATION', 'true') in ('1', 'True', 'true')
+SERVICE_ACCOUNT_CLIENT_ID = os.getenv('SERVICE_ACCOUNT_CLIENT_ID', 'not-set')
 
 BAYESIAN_DATA_IMPORTER_SERVICE_PORT = os.getenv(
     "BAYESIAN_DATA_IMPORTER_SERVICE_PORT", 9192)
@@ -36,3 +38,10 @@ DATA_IMPORTER_ENDPOINT = "http://%s:%s" % (
     BAYESIAN_DATA_IMPORTER_SERVICE_PORT)
 
 SENTRY_DSN = os.environ.get("SENTRY_DSN", "")
+
+USER_CACHE_DIR = os.environ.get("USER_CACHE_DIR")
+GEMINI_API_URL = "http://{host}:{port}/api/v1/pgsql".format(
+    host=os.environ.get("GEMINI_SERVICE_HOST", "f8a-gemini-server"),
+    port=os.environ.get("GEMINI_SERVICE_PORT", "5000"))
+ENABLE_USER_CACHING = os.environ.get('ENABLE_USER_CACHING', 'true') == 'true'
+ACCOUNT_SECRET_KEY = os.getenv('THREESCALE_ACCOUNT_SECRET', 'not-set')
