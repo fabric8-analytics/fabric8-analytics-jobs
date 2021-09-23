24ae883e0244135893550f25379ed3806479285a
jparsai <jparsai@redhat.com>
2021-09-23 18:52:28 +0530

    chore: revert debugging. (#437)

--- f8a_jobs/handlers/maven_releases.py
+++ f8a_jobs/handlers/maven_releases.py
@@ -45,7 +45,7 @@ class MavenReleasesAnalyses(BaseHandler):
             self.log.info('Timestamp is missing, we need to build the index from scratch.')
             pass
 
-        last_offset = 22383000
+        last_offset = s3.get_last_offset()
         self.log.info("{}__:__{}__:__{}".
                       format("MavenReleasesAnalyses", "last_offset", last_offset))
 
